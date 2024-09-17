f124fae39b0107ff91159d1ef79e56f35384b225
saratha kanagasabai <167672264+saratha91@users.noreply.github.com>
2024-09-17 23:28:36 +0530

    Requesting Squash merge from branch (Feature/lead) to prestaging branch (#1)
    * Implement controller for lead feature
    
    * Add DB model connectors for lead feature
    
    * Include lead feature view templates

---
+++ controller/lead/__init__.py
@@ -0,0 +1,16 @@
+# Lead feature implementation.
+ 
+class lead():
+ 
+  def __init__():
+    print 'Lead class constructor.'
+ 
+  def create():
+    print 'Lead created.'
+ 
+  def update():
+    print 'Lead updated.'
+ 
+  def view():
+    print 'View lead.'
+
